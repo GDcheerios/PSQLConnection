5016fee6e626f20e7f9f996e296189b544f4d2d4
GDcheerios <minecreeper0913@gmail.com>
2024-12-03 01:41:08 -0600

    added setup.py

---
+++ README.md
@@ -0,0 +1,7 @@
+# PSQLConnection
+## A wrapper I wrote to make simple psql queries
+
+---
+# Usage
+```py
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PSQLConnection',
+    version='0.0.1',
+    packages=['PSQLConnection'],
+    url='https://github.com/GDcheeriosYT/PSQLConnector',
+    license='UNKOWN',
+    author='GDcheerios',
+    author_email='minecreeper0913@gmail.com',
+    description='simple PSQL query wrapper'
+)
