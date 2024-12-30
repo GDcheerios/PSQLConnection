d59eb644a8d783d9c16694ab537df5ceb414e730
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2024-12-30 16:50:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='PSQLConnection',
     version='0.0.1',
     packages=['PSQLConnection'],
-    url='https://github.com/GDcheeriosYT/PSQLConnector',
+    url='https://github.com/GDcheeriosYT/PSQLConnection',
     license='UNKOWN',
     author='GDcheerios',
     author_email='minecreeper0913@gmail.com',
