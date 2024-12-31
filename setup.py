eef4dc37d44fb34f857427a965180a29bc4c6be6
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2024-12-31 15:58:13 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='PSQLConnection',
     version='0.0.1',
-    packages=['PSQLConnector', 'psycopg2'],
+    packages=['PSQLConnector'],
+    install_requires=['psycopg2'],
     url='https://github.com/GDcheeriosYT/PSQLConnection',
     license='UNKOWN',
     author='GDcheerios',
