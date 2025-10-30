00a2dd00181cc4df50255d0de321c46b8fe8fb64
Brayden Messerschmidt <minecreeper0913@gmail.com>
2025-10-30 15:09:03 -0500

    switch to binary

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='PSQLConnection',
     version='1.3.4',
     packages=['PSQLConnector'],
-    install_requires=['psycopg2'],
+    install_requires=['psycopg2-binary'],
     url='https://github.com/GDcheeriosYT/PSQLConnection',
     license='UNKOWN',
     author='GDcheerios',
