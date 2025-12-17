60e35bbfaaa0d9eed2b9a5a5c627e6af4f0ac1d9
Brayden Messerschmidt <minecreeper0913@gmail.com>
2025-12-17 12:16:41 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PSQLConnection',
-    version='1.3.4',
+    version='1.3.5',
     packages=['PSQLConnector'],
     install_requires=['psycopg2-binary'],
     url='https://github.com/GDcheeriosYT/PSQLConnection',
